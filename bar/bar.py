dbdac58897e9efd4d761e89c523aa6fc338fe17b
Otavio F <otaviofbrito@gmail.com>
2024-08-09 12:52:22 -0300

    transfer types

--- bar/bar.py
+++ bar/bar.py
@@ -1,18 +1,32 @@
 import mysql.connector
 import pandas as pd
-import plotly.express as px
+import seaborn as sns
+import matplotlib.pyplot as plt
+
 
 connection = mysql.connector.connect(
-        host='localhost',
-        user='user',
-        password='user',
-        database='tm_db',
-        port=3307
-    )
+    host='localhost',
+    user='user',
+    password='user',
+    database='tm_db',
+    port=3307
+)
 
-query = "SELECT COUNT(id) AS qt, country FROM leagues GROUP BY country;"
+query = """
+SELECT COUNT(*) AS transfers, year FROM transfers
+WHERE year > 1900
+GROUP BY(year)
+ORDER BY(year);
+"""
 
 df = pd.read_sql(query, connection)
 
+
 connection.close()
 
+# fig = px.bar(df, x='year', y='transfers')
+# fig.show()
+
+
+sns.barplot(data=df, x='year', y='transfers')
+plt.savefig("output_plot.png")
---
+++ bar/bar_transfer_type.py
@@ -0,0 +1,42 @@
+import mysql.connector
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import plotly.express as px
+import matplotlib.patches as mpatches
+
+connection = mysql.connector.connect(
+    host='localhost',
+    user='user',
+    password='user',
+    database='tm_db',
+    port=3307
+)
+
+query = """
+SELECT COUNT(*) AS transfers, transfer_type AS type FROM tm_db.transfers
+WHERE year > 1900
+GROUP BY(transfer_type);
+"""
+
+df = pd.read_sql(query, connection)
+
+
+connection.close()
+
+# fig = px.bar(df, x='type', y='transfers')
+# fig.show()
+
+palette = sns.color_palette("mako", len(df['type'].unique()))
+
+sns.barplot(data=df, x='type', y='transfers', hue='transfers', palette=palette)
+legend_labels = [f'{t} ({df[df["type"] == t]["transfers"].sum()/1e6:.2f}m)' for t in df['type'].unique()]
+handles = [mpatches.Patch(color=palette[i], label=legend_labels[i]) for i in range(len(legend_labels))]
+plt.legend(handles=handles)
+
+plt.xlabel("Type of Transfer")  # X-axis label
+plt.ylabel("Number of Transfers")  # Y-axis label
+plt.title("Distribution of Transfer Types")
+
+
+plt.savefig("transfer_type.png")
